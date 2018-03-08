8929efd50acc29ebf3bc450cc72880283e4041aa
Hackbright Student <no-reply@hackbrightacademy.com>
2018-03-08 12:01:36 -0800

    4 tests passed

--- list_operations.py
+++ list_operations.py
@@ -42,7 +42,7 @@ def tail(input_list):
 
     """
 
-    return []
+    return input_list[1:]
 
 
 def last(input_list):
@@ -55,7 +55,8 @@ def last(input_list):
 
     """
 
-    return []
+    return input_list[len(input_list)-1]
+    # return input_list[-1]
 
 
 def init(input_list):
@@ -68,7 +69,7 @@ def init(input_list):
 
     """
 
-    return []
+    return input_list[0:-1]
 
 
 ##############################################################################
